49efa9480db6cbe2aa35c29dbc6df1fae3ebc7c4
imanejarrari <benjarrariimane@gmail.com>
2024-11-28 17:20:22 +0100

    added home component

--- FrontEnd/src/app/home-component/home-component.component.ts
+++ FrontEnd/src/app/home-component/home-component.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
-import { SideComponentComponent } from "../side-component/side-component.component";
 import { CommonModule } from '@angular/common';
+import {SideComponentComponent} from "../side-component/side-component.component";
 
 
 @Component({
