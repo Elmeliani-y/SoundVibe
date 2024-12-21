ab6554f6b77e1f95e403516ca20a43c4e00d8105
imanejarrari <benjarrariimane@gmail.com>
2024-12-21 22:38:40 +0100

    :

--- BackEnd/User/connect.js
+++ BackEnd/User/connect.js
@@ -1,6 +1,7 @@
 const dotenv = require('dotenv');
 dotenv.config();
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const MONGODB_URL = process.env.MONGODB_URL;
 console.log('MONGODB_URL:', MONGODB_URL); 
@@ -8,28 +9,74 @@ if (!MONGODB_URL) {
     throw new Error("MONGODB_URL is not defined in .env file");
 }
 
-mongoose.connect(MONGODB_URL,)
+mongoose.connect(MONGODB_URL)
     .then(() => console.log("Connected to MongoDB"))
     .catch(err => console.error("Failed to connect to MongoDB:", err));
 
 const UserSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    lastname: { type: String, required: true, unique: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    musicStyle: { type: String },
-    profilePicture: { 
+    name: { 
         type: String, 
-        default: null 
+        required: [true, 'Name is required'],
+        trim: true
     },
-    favArtists: [{ 
-        name: String,
-        image: String 
+    lastname: { 
+        type: String, 
+        required: [true, 'Last name is required'],
+        trim: true
+    },
+    email: { 
+        type: String, 
+        required: [true, 'Email is required'],
+        unique: true,
+        trim: true,
+        lowercase: true,
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please enter a valid email']
+    },
+    password: { 
+        type: String, 
+        required: [true, 'Password is required'],
+        minlength: [6, 'Password must be at least 6 characters']
+    },
+    musicStyle: { 
+        type: String,
+        trim: true,
+        default: ''
+    },
+    profilePicture: { 
+        type: String,
+        default: 'default-profile.jpg'
+    },
+    favArtists: [{
+        type: String,
+        ref: 'Artist'
     }],
-    likedPlaylists: [{ 
-        name: String,
-        image: String 
+    likedPlaylists: [{
+        type: String,
+        ref: 'Playlist'
     }]
+}, { 
+    timestamps: true,
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true }
+});
+
+// Hash password before saving
+UserSchema.pre('save', async function(next) {
+    if (this.isModified('password')) {
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+    }
+    next();
+});
+
+// Method to compare password
+UserSchema.methods.comparePassword = async function(candidatePassword) {
+    return bcrypt.compare(candidatePassword, this.password);
+};
+
+// Virtual for user's full name
+UserSchema.virtual('fullName').get(function() {
+    return `${this.name} ${this.lastname}`;
 });
 
 const User = mongoose.model('User', UserSchema);
