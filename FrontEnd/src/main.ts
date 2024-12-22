1a97112e9dabf9ea5bebd1c03a9696597959ae6b
Safaabe <bahassounesafa66@gmail.com>
2024-12-22 18:15:14 +0100

    .

--- FrontEnd/src/main.ts
+++ FrontEnd/src/main.ts
@@ -3,6 +3,7 @@ import { provideAnimations } from '@angular/platform-browser/animations';
 import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
+import { provideHttpClient } from '@angular/common/http';
 
 bootstrapApplication(AppComponent, {
   providers: [
